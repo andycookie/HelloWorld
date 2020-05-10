742e8b365f73bbae73ad589e168d75038cd4b793
andycookie <781716048@qq.com>
2020-05-11 00:02:03 +0800

    first web HelloWorld

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * @Auther: http://www.pingan.com
+ * @Date: 2020/5/10
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Hello {
+	public static void main(String [] arr){
+		getMaxValue("xiahui");
+		System.out.println("we can do it");
+	
+
+	}
+	
+	public static void getMaxValue(String name){
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
